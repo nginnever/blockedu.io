a72be653222259ed97abcfb428468ef81a7af555
nginnever <ginneversource@gmail.com>
2017-06-04 19:31:23 -0700

    style

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	/* Popup */
+	$(".popup_1_show").click(function(){
+		$("#popup_1").show();
+		return false;
+	});
+	
+	$(".popup_1_hide").click(function(){
+		$("#popup_1").hide();
+		return false;
+	});
+});
